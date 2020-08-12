dcfa373ef663fbed19c210c0b9838e9d0592adb4
Alex Crouch <acrouch06@gmail.com>
2020-08-11 22:30:49 -0500

    updated MONGODB_URI

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
